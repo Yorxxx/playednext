58fdf15a7817ac96471d53248311698cd9be1c62
Jorge Garcia <jorge.garcia@bitmonlab.com>
2017-02-10 10:43:43 +0100

    [ADDED] Collection entity

---
+++ app/src/main/java/com/piticlistudio/playednext/collection/model/entity/Collection.java
@@ -0,0 +1,19 @@
+package com.piticlistudio.playednext.collection.model.entity;
+
+import com.google.auto.value.AutoValue;
+import com.piticlistudio.playednext.utils.AutoGson;
+
+/**
+ * Entity that defines a collection
+ * Created by jorge.garcia on 10/02/2017.
+ */
+@AutoValue
+public abstract class Collection {
+
+    public abstract int id();
+    public abstract String name();
+
+    public static Collection create(int id, String name) {
+        return new AutoValue_Collection(id, name);
+    }
+}
