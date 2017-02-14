48fc57d5d3271933a1355f5c14274e5e2bcf3f44
Jorge Garcia <jorge.garcia@bitmonlab.com>
2017-02-14 13:08:16 +0100

    [ADDED] IGenreData definitions

---
+++ app/src/main/java/com/piticlistudio/playednext/IGenreData.java
@@ -0,0 +1,23 @@
+package com.piticlistudio.playednext;
+
+/**
+ * Requirement methods for genre datasources
+ * Created by jorge.garcia on 14/02/2017.
+ */
+
+public interface IGenreData {
+
+    /**
+     * Returns the id
+     *
+     * @return the id
+     */
+    int getId();
+
+    /**
+     * Returns the name
+     *
+     * @return the name
+     */
+    String getName();
+}
