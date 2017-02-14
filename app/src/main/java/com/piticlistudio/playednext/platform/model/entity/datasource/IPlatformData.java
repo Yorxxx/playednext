f94aab8894f0abc136d1a4a4bfead9e9902dceca
Jorge Garcia <jorge.garcia@bitmonlab.com>
2017-02-14 17:44:20 +0100

    [ADDED] IPlatformData protocol

---
+++ app/src/main/java/com/piticlistudio/playednext/platform/model/entity/datasource/IPlatformData.java
@@ -0,0 +1,23 @@
+package com.piticlistudio.playednext.platform.model.entity.datasource;
+
+/**
+ * Definition methods
+ * Created by jorge.garcia on 14/02/2017.
+ */
+
+public interface IPlatformData {
+
+    /**
+     * Returns the id
+     *
+     * @return the id
+     */
+    int getId();
+
+    /**
+     * Returns the name
+     *
+     * @return the name
+     */
+    String getName();
+}
