ba9f345942cc61dfd406dd28b92533fd7e1f3734
Jorge Garcia <jorge.garcia@bitmonlab.com>
2017-02-14 10:48:34 +0100

    [ADDED] RealmCompanyMapper

---
+++ app/src/main/java/com/piticlistudio/playednext/company/model/entity/RealmCompanyMapper.java
@@ -0,0 +1,32 @@
+package com.piticlistudio.playednext.company.model.entity;
+
+import com.fernandocejas.arrow.optional.Optional;
+import com.piticlistudio.playednext.company.model.entity.datasource.RealmCompany;
+import com.piticlistudio.playednext.mvp.model.entity.Mapper;
+
+import javax.inject.Inject;
+
+/**
+ * Maps a Company into a RealmCompany
+ * Created by jorge.garcia on 14/02/2017.
+ */
+
+public class RealmCompanyMapper implements Mapper<RealmCompany, Company> {
+
+    @Inject
+    public RealmCompanyMapper() {
+    }
+
+    /**
+     * Transforms the data into an Optional model.
+     *
+     * @param data the data to transform
+     * @return an Optional model.
+     */
+    @Override
+    public Optional<RealmCompany> transform(Company data) {
+        if (data == null)
+            return Optional.absent();
+        return null;
+    }
+}
---
+++ app/src/test/java/com/piticlistudio/playednext/company/model/entity/RealmCompanyMapperTest.java
@@ -0,0 +1,34 @@
+package com.piticlistudio.playednext.company.model.entity;
+
+import com.fernandocejas.arrow.optional.Optional;
+import com.piticlistudio.playednext.BaseTest;
+import com.piticlistudio.playednext.company.model.entity.datasource.RealmCompany;
+
+import org.junit.Test;
+import org.mockito.InjectMocks;
+
+import static org.junit.Assert.*;
+
+/**
+ * Test cases
+ * Created by jorge.garcia on 14/02/2017.
+ */
+public class RealmCompanyMapperTest extends BaseTest {
+
+    @InjectMocks
+    RealmCompanyMapper mapper;
+
+    @Test
+    public void transform() throws Exception {
+
+        Company data = null;
+
+        // Act
+        Optional<RealmCompany> result = mapper.transform(data);
+
+        // Assert
+        assertNotNull(result);
+        assertFalse(result.isPresent());
+    }
+
+}
