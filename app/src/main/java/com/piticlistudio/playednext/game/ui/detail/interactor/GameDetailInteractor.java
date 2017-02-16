23c5c1032ad05e9bacbef82f1c7ac4573d446d1a
Jorge Garcia <jorge.garcia@bitmonlab.com>
2017-02-16 10:13:55 +0100

    [ADDED] GameDetailInteractor

---
+++ app/src/main/java/com/piticlistudio/playednext/game/ui/detail/interactor/GameDetailInteractor.java
@@ -0,0 +1,35 @@
+package com.piticlistudio.playednext.game.ui.detail.interactor;
+
+import com.piticlistudio.playednext.game.model.entity.Game;
+import com.piticlistudio.playednext.game.model.repository.IGameRepository;
+import com.piticlistudio.playednext.game.ui.detail.GameDetailContract;
+
+import javax.inject.Inject;
+
+import io.reactivex.Observable;
+
+/**
+ * Interactor implementation
+ * Created by jorge.garcia on 15/02/2017.
+ */
+
+public class GameDetailInteractor implements GameDetailContract.Interactor {
+
+    private final IGameRepository repository;
+
+    @Inject
+    public GameDetailInteractor(IGameRepository repository) {
+        this.repository = repository;
+    }
+
+    /**
+     * Loads the game with the specified id.
+     *
+     * @param id the id to load
+     * @return an Observable that emits the loaded data
+     */
+    @Override
+    public Observable<Game> load(int id) {
+        return repository.load(id);
+    }
+}
