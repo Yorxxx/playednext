e5787d631d8816fd513f9a71c1dc675fd06a10e7
Jorge Garcia <jorge.garcia@bitmonlab.com>
2017-03-08 17:10:45 +0100

    Started boost item module

---
+++ app/src/main/java/com/piticlistudio/playednext/boost/model/entity/BoostTypes.java
@@ -0,0 +1,10 @@
+package com.piticlistudio.playednext.boost.model.entity;
+
+/**
+ * Type definitions for every boost
+ * Created by jorge.garcia on 08/03/2017.
+ */
+public class BoostTypes {
+
+    public final static int BOOST_GAME_RECENTLY_RELEASED = 1;
+}
---
+++ app/src/main/java/com/piticlistudio/playednext/boost/model/entity/IBoostable.java
@@ -0,0 +1,16 @@
+package com.piticlistudio.playednext.boost.model.entity;
+
+/**
+ * An interface defining boostable items
+ * Created by jorge.garcia on 08/03/2017.
+ */
+
+public interface IBoostable {
+
+    /**
+     * Returns the last release
+     *
+     * @return the last release
+     */
+    long getLastRelease();
+}
