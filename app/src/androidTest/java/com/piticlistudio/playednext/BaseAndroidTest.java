17c760a352177f5ab20bba08e5612eb4475b8e86
Jorge <jorgegarciacebria@gmail.com>
2017-05-10 20:20:47 +0200

    Fixes AppNotIdleException on ProgressBar
    Testing Espresso against a ProgressBar causes the view to draw
    indefinitely, and running into AppNotIdleException.
    See http://stackoverflow.com/questions/33289152/progressbars-and-espresso

--- app/src/androidTest/java/com/piticlistudio/playednext/BaseAndroidTest.java
+++ app/src/androidTest/java/com/piticlistudio/playednext/BaseAndroidTest.java
@@ -1,9 +1,19 @@
 package com.piticlistudio.playednext;
 
+import android.graphics.drawable.ColorDrawable;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.UiController;
+import android.support.test.espresso.ViewAction;
+import android.view.View;
+import android.widget.ProgressBar;
+
+import org.hamcrest.Matcher;
 
 import io.reactivex.functions.Consumer;
 import io.reactivex.functions.Predicate;
+
+import static android.support.test.espresso.action.ViewActions.actionWithAssertions;
+import static android.support.test.espresso.matcher.ViewMatchers.isAssignableFrom;
 
 /**
  * Base Android Junit tests
@@ -21,5 +31,27 @@ public class BaseAndroidTest {
     protected AndroidApplication getApp() {
         return (AndroidApplication) InstrumentationRegistry.getInstrumentation()
                 .getTargetContext().getApplicationContext();
+    }
+
+    public static ViewAction replaceProgressBarDrawable() {
+        return actionWithAssertions(new ViewAction() {
+            @Override
+            public Matcher<View> getConstraints() {
+                return isAssignableFrom(ProgressBar.class);
+            }
+
+            @Override
+            public String getDescription() {
+                return "replace the ProgressBar drawable";
+            }
+
+            @Override
+            public void perform(final UiController uiController, final View view) {
+                // Replace the indeterminate drawable with a static red ColorDrawable
+                ProgressBar progressBar = (ProgressBar) view;
+                progressBar.setIndeterminateDrawable(new ColorDrawable(0xffff0000));
+                uiController.loopMainThreadUntilIdle();
+            }
+        });
     }
 }
